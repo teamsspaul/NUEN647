419507406dd5c406fdbdf0d36f280ba00ef3011c
Paul Mendoza <paul.m.mendoza@gmail.com>
2016-10-03 13:12:10 -0500

    Update Files

--- Assignments/Homework1/Problem9/Functions.py
+++ Assignments/Homework1/Problem9/Functions.py
@@ -37,14 +37,7 @@ import Functions as fun
 ######################### Functions ############################
 ################################################################
 
-def GammaPDF(alpha,beta,theta):
-    f_x=((theta**(alpha-1))*np.exp(-theta*beta))\
-                           /\
-        (sps.gamma(alpha)*beta**(-alpha))
-    return(f_x)
-
-
-def Plot(theta,f_x):
+def PlotSetup():
     
     fig = plt.figure()
     ax = [fig.add_subplot(111)]
@@ -56,66 +49,40 @@ def Plot(theta,f_x):
                       labelleft='off',labelright='off',labeltop='off',
                       labelbottom='off',right='off')
                     
-    ax = np.append(ax,fig.add_subplot(211))
-    ax = np.append(ax,fig.add_subplot(212))
+    ax = np.append(ax,fig.add_subplot(411)) #1
+    ax = np.append(ax,fig.add_subplot(412)) #2
+    ax = np.append(ax,fig.add_subplot(421)) #3
+    ax = np.append(ax,fig.add_subplot(422)) #4
     
-    ax[1].plot(theta,f_x,'r',linewidth=2.0)
-    ax[2].plot(theta,f_x,'r',linewidth=2.0)
-    
-    ax[2].set_xlabel(r'$\boldsymbol{\theta}$',fontsize=18)
-    ax[0].set_ylabel(r'\textbf{Probability}',fontsize=18)
-    ax[0].yaxis.labelpad=55
+    ax[0].set_xlabel(r'Number of Samples',fontsize=18)
+    ax[0].xaxis.labelpad=20
 
-    ax[1].set_ylabel(r'Square $N=10^3$',fontsize=14)
-    ax[2].set_ylabel(r'Triangular $N=10^3/2$',fontsize=14)
+    ax[1].set_ylabel(r'Mean',fontsize=14)
+    ax[2].set_ylabel(r'Variance',fontsize=14)
+    ax[3].set_ylabel(r'Skew',fontsize=14)
+    ax[4].set_ylabel(r'Kurtosis',fontsize=14)
     
     ax[1].xaxis.set_tick_params(labelsize=14)
     ax[1].yaxis.set_tick_params(labelsize=14)
     ax[2].xaxis.set_tick_params(labelsize=14)
     ax[2].yaxis.set_tick_params(labelsize=14)
+    ax[3].xaxis.set_tick_params(labelsize=14)
+    ax[3].yaxis.set_tick_params(labelsize=14)
+    ax[4].xaxis.set_tick_params(labelsize=14)
+    ax[4].yaxis.set_tick_params(labelsize=14)
     
     ax[1].grid(alpha=0.8,color='black',linestyle='dotted')
     ax[2].grid(alpha=0.8,color='black',linestyle='dotted')
+    ax[3].grid(alpha=0.8,color='black',linestyle='dotted')
+    ax[4].grid(alpha=0.8,color='black',linestyle='dotted')
     return(fig,ax)
 
-def PlotaxIn(X,Y,i,ax,j):
-    if i<10**3 and j==1:
-        ax[j].plot(X,Y,'x',markersize=7,markeredgewidth=2.5,
-                   color='gray')
-    elif i<(10**3)/2 and j==2:
-        ax[j].plot(X,Y,'x',markersize=7,markeredgewidth=2.5,
-                   color='gray',label="Accepted")
+def Plot(N,mean,variance,skew,kurtosis,ax):
+        
+    ax[1].plot(N,mean,'.k')
+    ax[2].plot(N,variance,'.k')
+    ax[3].plot(N,skew,'.k')
+    ax[4].plot(N,kurtosis,'.k')
+
     return(ax)
 
-def PlotaxOut(X,Y,i,ax,j):
-    if i<10**3 and j==1:
-        ax[j].plot(X,Y,'ko',markersize=5)
-    elif i<(10**3)/2 and j==2:
-        ax[j].plot(X,Y,'ko',markersize=5,label="Rejected")
-    return(ax)
-
-def Plotlegend(ax,theta,f_x):
-    handles,labels=ax[2].get_legend_handles_labels()
-    Outside=False
-    Inside=False
-
-    for i in range(0,len(handles)):
-        if Outside and Inside:
-            break
-        if not Outside and "Rejected" in labels[i]:
-            Outside=True
-            OutsideIndex=i
-        if not Inside and "Accepted" in labels[i]:
-            Inside=True
-            InsideIndex=i
-
-    Smallerhandles=[handles[OutsideIndex],handles[InsideIndex]]
-    Smallerlabels=[labels[OutsideIndex],labels[InsideIndex]]
-    Lfont={'family':'monospace',
-             'size': 12}
-    ax[2].legend(Smallerhandles,Smallerlabels,
-                 loc='best',fontsize=12,prop=Lfont)
-
-    ax[1].plot(theta,f_x,'r',linewidth=2.0)
-    ax[2].plot(theta,f_x,'r',linewidth=2.0)
-    return(ax)
