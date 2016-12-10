3190a5576adc649a20a99166257d6a689d51e119
Paul Mendoza <paul.m.mendoza@gmail.com>
2016-12-09 22:54:47 -0600

    "Update Files"

---
+++ Assignments/Homework3/Problem3/Functions.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+"""
+FractionAM converts atom fractions to mass fractions
+and mass fractions to atom fractions. Input is a 
+single string with MCNP style fractions.
+"""
+
+__author__     =  "Paul Mendoza"
+__copyright__  =  "Copyright 2016, Planet Earth"
+__credits__    = ["Sunil Chirayath",
+                  "Charles Folden",
+                  "Jeremy Conlin"]
+__license__    =  "GPL"
+__version__    =  "1.0.1"
+__maintainer__ =  "Paul Mendoza"
+__email__      =  "paul.m.mendoza@gmail.com"
+__status__     =  "Production"
+
+################################################################
+##################### Import packages ##########################
+################################################################
+
+import os.path
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime
+from uncertainties import ufloat
+from uncertainties.umath import *
+from uncertainties import unumpy as unp
+import re
+
+################################################################
+######################### Functions ############################
+################################################################
+
+
