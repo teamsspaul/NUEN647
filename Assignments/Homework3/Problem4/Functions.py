57b0388021c8ed921b7d327f0ada6d7ec7070a8b
Paul Mendoza <paul.m.mendoza@gmail.com>
2016-12-10 21:22:52 -0600

    "Update Files"

--- Assignments/Homework3/Problem4/Functions.py
+++ Assignments/Homework3/Problem4/Functions.py
@@ -21,6 +21,10 @@ __status__     =  "Production"
 ##################### Import packages ##########################
 ################################################################
 
+import numpy as np
+import matplotlib.pyplot as plt
+import lhsmdu
+from scipy.stats import beta
 
 ################################################################
 ######################### Functions ############################
@@ -36,8 +40,10 @@ def X(t):
 def Y(s):
     return(2*s-1)
 
-#Van der Corput sequence function found online
 def vdc(n, base=2):
+    """
+    Generates van der Corput sequence function
+    """
     vdc, denom = 0,1
     while n:
         denom *= base
@@ -45,3 +51,45 @@ def vdc(n, base=2):
         vdc += remainder / denom
     return vdc
 
+def Rstrat(N,Nstrata):
+    """
+    Generate a vector N long with stratified
+    random variables, with Nstrata bins
+    """
+    RN=[]
+    Nloop=int(N/Nstrata)*Nstrata
+    for i in range(0,int(N/Nstrata)):
+        for j in range(0,Nstrata):
+            RN.append(np.random.uniform(low=j/Nstrata,
+                                high=(j+1)/Nstrata,size=1))
+    #If N/Nstrata doesn't divide evenly
+    if Nloop<N:
+        for j in range(0,N-Nloop):
+            RN.append(np.random.uniform(low=j/Nstrata,
+                            high=(j+1)/Nstrata,size=1))
+    return(RN)
+
+#Van Sampling
+def Rvdc(N,base=2):
+    """
+    Will generate a vector N long of the van
+    der Corput sequence with a chosen base
+    """
+    RN=[]
+    for i in range(0,N):
+        RN.append(vdc(i+1,base))
+    return(RN)
+
+def HIST(Xlabel,Samples,Nbins,filename,N):
+    """
+    Plot histogram
+    """
+    fig=plt.figure()
+    ax=fig.add_subplot(111)
+    ax.set_xlabel(Xlabel,fontsize=16)
+    weights=np.ones_like(Samples)/len(Samples)
+    ax.set_ylabel('Normalized counts with tot N = '+str(N),fontsize=18)
+    ax.hist(Samples,Nbins,
+            weights=weights,color='green',alpha=0.7,edgecolor='black')
+    #ax.set_xlim(-500,500)
+    plt.savefig(filename)
