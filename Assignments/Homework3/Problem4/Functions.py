642ee58e81b3ba710a0be934954c8fadb55f12cd
Paul Mendoza <paul.m.mendoza@gmail.com>
2016-12-11 14:06:00 -0600

    "Update Files"

--- Assignments/Homework3/Problem4/Functions.py
+++ Assignments/Homework3/Problem4/Functions.py
@@ -25,14 +25,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 import lhsmdu
 from scipy.stats import beta
+from scipy import interpolate
+from scipy import integrate
+from scipy.integrate import trapz
 
 ################################################################
 ######################### Functions ############################
 ################################################################
 
+def betau(a,b):
+    return(a/(a+b))
+
+def betao(a,b):
+    return((a*b)/(((a+b)**2)*(a+b+1)))
 
 def Rosen(x,y):
     return((1-x)**2+100*(y-x**2)**2)
+
+def Z(x,y):
+    return(10-Rosen(x,y))
+
 
 def X(t):
     return(2*t-1)
@@ -84,12 +96,41 @@ def HIST(Xlabel,Samples,Nbins,filename,N):
     """
     Plot histogram
     """
+    #Plot histogram
     fig=plt.figure()
     ax=fig.add_subplot(111)
     ax.set_xlabel(Xlabel,fontsize=16)
-    weights=np.ones_like(Samples)/len(Samples)
+    #For normalization of histogram
+    weights=np.ones_like(Samples)/(len(Samples))
     ax.set_ylabel('Normalized counts with tot N = '+str(N),fontsize=18)
-    ax.hist(Samples,Nbins,
+    #Get data from histogram and plot
+    n,bins,patches=ax.hist(Samples,Nbins,
             weights=weights,color='green',alpha=0.7,edgecolor='black')
-    #ax.set_xlim(-500,500)
+
+    #Modify some of the data from histograms
+    Probability=n #Probability in a particular bin
+    XValues=[]
+    PDF=[]   #Probability density function (different than Prob)
+    for i in range(0,len(n)):
+        XValues.append((bins[i]+bins[i+1])/2)
+        PDF.append(Probability[i]/(bins[i+1]-bins[i]))
+        
+    #Fit the data to a function
+    m=(PDF[1]-PDF[0])/(XValues[1]-XValues[0]) #slope at start
+    b=PDF[0]-m*XValues[0]
+    #print(b,PDF[0],PDF[1])
+    XValues.insert(0,0)
+    PDF.insert(0,b)
+    I=interpolate.interp1d(XValues,PDF,
+                           fill_value=max(Probability),bounds_error=False)
+    #Evaluate the function and see how well it
+    #fits the data
+    x=np.linspace(min(XValues),max(XValues),10000)
+    x=np.linspace(min(XValues),10,10000)
+    y=I(x)
+    ax.plot(x,y,'r',linewidth=2.0)
     plt.savefig(filename)
+
+    #does the pdf integrate to 1?
+    print(integrate.trapz(I(x),x))
+    return(I)
